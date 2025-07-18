805f440b6436736b3c36be3d4b8cc973959ac034
Matito <77804268+MatitoDev@users.noreply.github.com>
2025-07-18 16:01:44 +0200

    error handling

--- src/main/java/dev/matito/forceItem/commands/StartCommand.java
+++ src/main/java/dev/matito/forceItem/commands/StartCommand.java
@@ -2,18 +2,26 @@ package dev.matito.forceItem.commands;
 
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.matito.forceItem.ForceItem;
-import dev.matito.forceItem.GameManager;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.CommandSender;
 
 public class StartCommand {
 	public static void start(CommandSender sender) {
+		if (ForceItem.INSTANCE.getGameManager().running) {
+			sender.sendMessage(ForceItem.getPrefix().append(Component.text("Game already running!", NamedTextColor.RED)));
+			return;
+		}
+		if (ForceItem.INSTANCE.getItemTable().getPlayersCurrentItems().isEmpty()) {
+			sender.sendMessage(ForceItem.getPrefix().append(Component.text("No Players added yet!", NamedTextColor.RED)));
+			return;
+		}
 		ForceItem.INSTANCE.getGameManager().start();
 	}
 
 	public static void register() {
 		new CommandAPICommand("start")
 				.withPermission("force-item.start")
-				//.withArguments(new StringArgument("arg0"))
 				.executes(((sender, args) -> {
 					start(sender);
 				}))
